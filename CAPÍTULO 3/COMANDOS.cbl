106b7d2c12bc050f3f25277710eb23f6480e8814
Luís Henrique do Amaral Gomes <147919735+LuisHGomes@users.noreply.github.com>
2024-11-13 16:51:29 -0300

    Capítulo 3 (Em desenvolvimento)

--- CAPÍTULO 3/COMANDOS.cbl
+++ CAPÍTULO 3/COMANDOS.cbl
@@ -9,13 +9,23 @@
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
+
        77 WS-NUM-1                 PIC 9(002) VALUE ZEROS.
        77 WS-NUM-2                 PIC 9(002) VALUE ZEROS.
        77 WS-RESULTADO             PIC 9(003) VALUE ZEROS.
        77 WS-NOVO-RESULT           PIC 9(004) VALUE ZEROS.
+       77 WS-RESPOSTA              PIC X(001) VALUE SPACES.
+
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
+
+       PERFORM S1.
+       PERFORM S2.
+
+       S1 SECTION.
             DISPLAY "OLA MUNDO !".
+
+       S2 SECTION.
             MOVE 1                 TO WS-NUM-1.
             MOVE 30                TO WS-NUM-2.
             DISPLAY "NUMERO 1 = " WS-NUM-1 " E NUMERO 2 = " WS-NUM-2.
@@ -23,10 +33,22 @@
        PERFORM FUNCAO THRU FIM-FUNCAO.
 
        FUNCAO.
-           COMPUTE WS-RESULTADO = WS-NUM-1 + WS-NUM-2.
+           COMPUTE WS-RESULTADO        =          WS-NUM-1 + WS-NUM-2.
            DISPLAY "RESULTADO DA SOMA = " WS-RESULTADO.
-           SET WS-NOVO-RESULT      TO WS-RESULTADO.
+           SET WS-NOVO-RESULT          TO          WS-RESULTADO.
            DISPLAY "NOVO RESULTADO = " WS-RESULTADO.
+           DISPLAY "DESEJA ENCERRAR O PROGRAMA : S/N ?".
+           ACCEPT WS-RESPOSTA.
+
+           IF WS-RESPOSTA EQUAL "S"
+               DISPLAY "ENCERRANDO PROGRAMA"
+               DISPLAY "ENCERRANDO PROGRAMA"
+           ELSE
+               GOBACK
+           END-IF.
+
        FIM-FUNCAO.
+
+
             STOP RUN.
        END PROGRAM COMANDOS.
